27d09e95fb48135825a98d666f4ce97b455bd355
glenn <glenn.jo@kakaocorp.com>
2017-02-21 17:21:25 +0900

    부트스트랩 테마를 이용하여 화면틀 작업

---
+++ js/style.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  //navigation hover style
+  $(".nav li a").hover(function(){
+    $(this).css('color', '#DAE2D8');
+  }, function(){
+    $(this).css('color', '#333333');
+  })
+});
+
+
+
+
